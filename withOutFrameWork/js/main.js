d11d7da92e8794a28e84ea7e22e832030f260727
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-03-26 20:16:24 +0600

    added swiper js

--- withOutFrameWork/js/main.js
+++ withOutFrameWork/js/main.js
@@ -1,10 +1,48 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+//   Show menu
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// Menu hide
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SWIPER SNEAKERS ===============*/
+let swiperImages = new Swiper(".home__swiper", {
+  loop: true,
+  spaceBetween: 64,
+  grabCursor: true,
+  centeredSliders: true,
 
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
